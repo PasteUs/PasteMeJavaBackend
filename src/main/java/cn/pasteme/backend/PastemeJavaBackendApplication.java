ba2b9723791250ad9addf848e470c666e660f5fb
Irene Liu <ireneliuqaq@qq.com>
2019-09-14 15:18:03 +0800

    Project init

---
+++ src/main/java/cn/pasteme/backend/PastemeJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.pasteme.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastemeJavaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastemeJavaBackendApplication.class, args);
+    }
+
+}
