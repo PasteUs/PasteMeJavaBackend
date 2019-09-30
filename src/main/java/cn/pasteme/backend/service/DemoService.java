8d7878503d11327b3d1057b2fe3bfe175c7aa97a
Lucien Shui <lucien@lucien.ink>
2019-10-01 01:59:07 +0800

    Update time format

--- src/main/java/cn/pasteme/backend/service/DemoService.java
+++ src/main/java/cn/pasteme/backend/service/DemoService.java
@@ -4,7 +4,7 @@ import cn.pasteme.common.dto.TokenDTO;
 import cn.pasteme.common.vo.ContentVO;
 
 /**
- * Created by 白振宇 on 2019/9/30 0:41
+ * Created by 白振宇 on 2019/9/30 00:41
  */
 public interface DemoService {
     ContentVO getContentByKey(TokenDTO tokenDTO);
