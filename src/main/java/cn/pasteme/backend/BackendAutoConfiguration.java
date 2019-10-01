52f8f519835515aca3552119ae618bf4f272ff93
Ryan Lee <gxlhybh@gmail.com>
2019-10-01 17:23:01 +0800

    Add author info

--- src/main/java/cn/pasteme/backend/BackendAutoConfiguration.java
+++ src/main/java/cn/pasteme/backend/BackendAutoConfiguration.java
@@ -4,6 +4,11 @@ import cn.pasteme.common.annotation.NotComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author Ryan Lee
+ * @date 2019/10/01 17:01
+ */
+
 @Configuration
 @ComponentScan({"cn.pasteme.backend", "cn.pasteme.common"})
 @NotComponentScan
--- src/main/java/cn/pasteme/backend/BackendDatasourceConfig.java
+++ src/main/java/cn/pasteme/backend/BackendDatasourceConfig.java
@@ -3,6 +3,11 @@ package cn.pasteme.backend;
 import cn.pasteme.common.annotation.NotComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author Ryan Lee
+ * @date 2019/10/01 17:01
+ */
+
 @NotComponentScan
 @Configuration
 public class BackendDatasourceConfig {
